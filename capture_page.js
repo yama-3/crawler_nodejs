c6fcdaf2164ecdb7d3d87529b6acd0c171e1e494
Kazuhiro Yamabe <kazuhiro.yamabe@gmail.com>
2016-08-08 11:55:24 +0900

    Add capture parameters

--- capture_page.js
+++ capture_page.js
@@ -7,7 +7,7 @@ var url = casper.cli.get("url");
 
 casper.start(url, function() {
     this.viewport(1024, 768);
-    this.capture("page.png");
+    this.capture("page.png", {top: 0, left: 0, width: 1024, height: 1000});
 });
 
 casper.run();
