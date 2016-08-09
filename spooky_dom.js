ad5977452200a0c34d7ab6d05a971e83b6e7bb79
Kazuhiro Yamabe <kazuhiro.yamabe@gmail.com>
2016-08-09 16:33:06 +0900

    Refactoring using SpookyJS, Remote getElementBySelector

--- spooky_dom.js
+++ spooky_dom.js
@@ -1,45 +1,44 @@
 var Spooky = require('spooky');
 var cheerio = require('cheerio');
 
-var spooky = new Spooky({
-	child: {
-	    transport: 'http'
-	},
-	casper: {
-	    logLevel: 'debug',
-	    verbose: true
-	}
-},
-			
-    function (err) {
-	if (err) {
-	    e = new Error('Failed to initialize SpookyJS');
-	    e.details = err;
-	    throw e;
-	}
+var URL = 'http://cs.crosswarp.com/wf/hoge/test/DomPage';
+var elementSelector = '#content2 li';
 
-	spooky.start('http://cs.crosswarp.com/wf/hoge/test/DomPage');
-	
-	spooky.then(function() {
-	    var innerHtml = this.evaluate(function() {
-		return document.querySelector('div#content2').innerHTML;
-	    });
-	    this.emit('target', innerHtml);
-	    });
-
-	spooky.run();
-    });
-
-spooky.on('target', function(innerHtml) {
-    console.log(innerHtml)
-    $ = cheerio.load(innerHtml);
-    $('li').each(function(i, elm) {
+var htmlFunc = function(html) {
+    console.log(html)
+    $ = cheerio.load(html);
+    $(elementSelector).each(function(i, elm) {
 	console.log($(this).text());
     });
-});
+};
 
-/*
-spooky.on('console', function (line) {
-    console.log(line);
-});
-*/
+var spookyCallback = function (err) {
+    if (err) {
+	e = new Error('Failed to initialize SpookyJS');
+	e.details = err;
+	throw e;
+    }
+	
+    spooky.start(URL);
+    spooky.then(function() {
+	var html = this.evaluate(function() {
+	    return document.documentElement.outerHTML;
+	});
+	this.emit('html', html);
+    });
+    spooky.run();
+
+    spooky.on('html', htmlFunc);
+    spooky.on('log', function(log) {
+	if (log.space === 'remote') {
+	    console.log(line);
+	}
+    });
+    spooky.on('console', function (line) {
+	console.log(line);
+    });
+};
+
+var options = {child: { transport: 'http' }, casper: { logLevel: 'debug', verbose: true }}
+var spooky = new Spooky(options, spookyCallback);
+
