846bad92d55b4b058a07801f8cff76bfe4334249
yama3 <kazuhiro.yamabe@gmail.com>
2016-11-03 20:07:02 +0900

    Return image file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log(req);
+
 	//res.render('index', { title: 'Express', message: "Hoge" });
-	res.json({'a': 1, 'b': 2.0});
+	//res.json({'a': 1, 'b': 2.0});
+	
+	res.sendFile(path.resolve('./sagan.jpeg'));
 });
 
 module.exports = router;
