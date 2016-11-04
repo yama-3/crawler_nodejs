37d95f2aa5b4e853343eab1c2e4b12ce8ef90f3b
yama3 <kazuhiro.yamabe@gmail.com>
2016-11-04 19:55:52 +0900

    Support POST

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,18 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	console.log(req);
+	//console.log(req);
 
 	//res.render('index', { title: 'Express', message: "Hoge" });
 	//res.json({'a': 1, 'b': 2.0});
 	
 	res.sendFile(path.resolve('./sagan.jpeg'));
+});
+
+// curl -XPOST localhost:3333/ -H 'Content-Type:application/json' -d '{"a": 1}'
+router.post('/', function(req, res) {
+	console.log(req.body);
+	res.json(req.body);
 });
 
 module.exports = router;
