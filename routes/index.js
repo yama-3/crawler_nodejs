702880a303f333ffff8dd7c2175d3d84b7a04d52
Kazuhiro Yamabe <kazuhiro.yamabe@gmail.com>
2016-11-08 14:37:38 +0900

    Create servce return web site capture

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+//var sleep = require('sleep-async')();
+var sleep = require('sleep');
+var fs = require('fs');
+var md5hash = require('../md5hash');
+var spookyCapture = require("../spooky_capture");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,10 +17,29 @@ router.get('/', function(req, res, next) {
 	res.sendFile(path.resolve('./sagan.jpeg'));
 });
 
-// curl -XPOST localhost:3333/ -H 'Content-Type:application/json' -d '{"a": 1}'
+// curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{"url": "http://www.yahoo.co.jp","selector": "#topicsbox"}' "http://localhost:3333"
 router.post('/', function(req, res) {
-	console.log(req.body);
-	res.json(req.body);
+    console.log(req.body);
+    var requestedAt = new Date();
+    var url = req.body['url'];
+    var selector = req.body['selector'];
+    var filename = './images/' + md5hash.md5hex(url + selector + requestedAt) + '.png';
+	
+    try {
+	fs.unlinkSync(filename);
+    } catch (e){
+	console.log(e);
+    }
+
+    spookyCapture.run(url, selector, filename);
+
+    fs.watchFile(filename, (curr, prev) => {
+	if (curr.mtime > prev.mtime) {
+	    res.sendFile(path.resolve(filename));
+	    fs.unwatchFile(filename);
+	}
+    });
+
 });
 
 module.exports = router;
